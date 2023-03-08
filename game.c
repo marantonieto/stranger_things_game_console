ffb9c282968ce7ffea5aecc2648b420d8b062c57
Marcio Antonio <marcio.asfilho16@gmail.com>
2023-03-08 20:35:58 -0300

    Sala vazia criada e ajustes nos nomes dos personagens

--- game.c
+++ game.c
@@ -4,8 +4,8 @@
 #include <time.h>
 #include <conio.h>
 
-#define HISOKA 'H'
-#define GON 'G'
+#define ELEVEN 'E'
+#define WILL 'W'
 #define CHROLLO 'C'
 #define EMPTY '_'
 #define ROCK '@'
@@ -15,15 +15,17 @@ typedef struct{
 
     int pos_x;
     int pos_y;
+    int life;
+    int rage;
 
-}Hisoka;
+}Eleven;
 
 typedef struct{
 
     int pos_x;
     int pos_y;
 
-}Gon;
+}Will;
 
 typedef struct{
 
@@ -48,73 +50,105 @@ typedef struct{
 
 typedef struct{
 
-    char** map;
-    Gon* gon;
+    char** upside_down;
+    char** empty_room;
+    Will* will;
     Chrollo* chrollo;
-    Hisoka* hisoka;
+    Eleven* eleven;
 
 }Game;
 
+void empty_room_generator(Game* game){
+    int i, j;
+    game->empty_room = (char**)malloc(10 * sizeof(char*));
+    for(i = 0; i < 10; i++){
+        game->empty_room[i] = (char*)malloc(10 * sizeof(char));
+    }
 
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+            game->empty_room[i][j] = EMPTY;
+        }
+    }
+}
 
-//MAP FUNCTIONS
-void map_generator(Game* game){
+void show_empty_room(Game* game){
+
+    system("cls");
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(game->eleven->pos_x == i && game->eleven->pos_y == j){
+                game->empty_room[i][j] = ELEVEN;
+                printf(" %c", game->empty_room[i][j]);
+            }
+            else{
+                printf(" %c", game->empty_room[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n\nLIFE: %d\nRAGE: %d\n", game->eleven->life, game->eleven->rage);
+}
+
+//UPSIDE FUNCTIONS
+void upside_down_generator(Game* game){
     int i, j;
 
-    game->map = (char**) malloc(15 * sizeof(char*));
+    game->upside_down = (char**) malloc(15 * sizeof(char*));
 
     for(i = 0; i < 15; i++){
-        game->map[i] = (char*) malloc(15 * sizeof(char));
+        game->upside_down[i] = (char*) malloc(15 * sizeof(char));
     }
 
     for(i = 0; i < 15; i++){
         for(j = 0; j < 15; j++){
-            game->map[i][j] = EMPTY;
+            game->upside_down[i][j] = EMPTY;
         }
     }
 
     for(int j = 0; j < 15; j++){
-        game->map[rand()%15][rand()%15] = TREE; 
-        game->map[rand()%15][rand()%15] = ROCK;
+        game->upside_down[rand()%15][rand()%15] = TREE; 
+        game->upside_down[rand()%15][rand()%15] = ROCK;
     }
 }
 
-void show_map(Game* game){
+void show_upside_down(Game* game){
 
     system("cls");
 
     for(int i = 0; i < 15; i++){
         for(int j = 0; j < 15; j++){
-            if(game->hisoka->pos_x == i && game->hisoka->pos_y == j){
-                game->map[i][j] = HISOKA;
+            if(game->eleven->pos_x == i && game->eleven->pos_y == j){
+                game->upside_down[i][j] = ELEVEN;
                 printf("\033[1;33m");
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
                 printf("\033[0m");
             }
-            else if(game->gon->pos_x == i && game->gon->pos_y == j){
-                game->map[i][j] = GON;
+            else if(game->will->pos_x == i && game->will->pos_y == j){
+                game->upside_down[i][j] = WILL;
                 printf("\033[1;36m");
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
                 printf("\033[0m");
             }
             else if(game->chrollo->pos_x == i && game->chrollo->pos_y == j){
-                game->map[i][j] = CHROLLO;
+                game->upside_down[i][j] = CHROLLO;
                 printf("\033[1;31m");
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
                 printf("\033[0m");
             }
-            else if(game->map[i][j] == TREE){
+            else if(game->upside_down[i][j] == TREE){
                 printf("\033[0;32m");
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
                 printf("\033[0m");
             }
-            else if(game->map[i][j] == ROCK){
+            else if(game->upside_down[i][j] == ROCK){
                 printf("\033[0;30m");
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
                 printf("\033[0m");   
             }
             else{
-                printf(" %c", game->map[i][j]);
+                printf(" %c", game->upside_down[i][j]);
             }
         }
         printf("\n");
@@ -125,78 +159,81 @@ void show_map(Game* game){
 
 //CHARACTERS CREATION
 
-Gon* create_gon(Gon* gon){
+Will* create_will(Will* will){
 
     do{
-        gon->pos_x = rand() % 15;
-        gon->pos_y = rand() % 15;
+        will->pos_x = rand() % 15;
+        will->pos_y = rand() % 15;
 
-    }while(gon->pos_x == 5 && gon->pos_y == 5);
+    }while(will->pos_x == 5 && will->pos_y == 5);
 
-    return gon;
+    return will;
 
 }
 
-Chrollo* create_chrollo(Chrollo* chrollo, Gon* gon){
+Chrollo* create_chrollo(Chrollo* chrollo, Will* will){
 
     do{
         chrollo->pos_x = rand() % 15;
         chrollo->pos_y = rand() % 15;
 
-    }while((chrollo->pos_x == gon->pos_x) && (chrollo->pos_y == gon->pos_y));
+    }while((chrollo->pos_x == will->pos_x) && (chrollo->pos_y == will->pos_y));
 
     return chrollo;
 
 }
 
 
-Hisoka* create_hisoka(Hisoka* hisoka){
+Eleven* create_eleven(Eleven* eleven){
 
-    hisoka->pos_x = 5;
-    hisoka->pos_y = 5;
+    eleven->pos_x = 5;
+    eleven->pos_y = 5;
+    eleven->life = 100;
+    eleven->rage = 0;
 
-    return hisoka;
+    return eleven;
 }
 
 //CHARACTERS MOVEMENT
 
-void hisoka_movement(Game* game){
-    game->map[game->hisoka->pos_x][game->hisoka->pos_y] = EMPTY;
+void eleven_movement(Game* game){
+    game->empty_room[game->eleven->pos_x][game->eleven->pos_y] = EMPTY;
     int button;
 
     button = getch();
 
     //UP
     if(button == 'W' || button == 'w'){
-        if(game->hisoka->pos_x - 1 >= 0 && game->map[game->hisoka->pos_x - 1][game->hisoka->pos_y] == EMPTY){
-            game->hisoka->pos_x--;
+        if(game->eleven->pos_x - 1 >= 0 && game->empty_room[game->eleven->pos_x - 1][game->eleven->pos_y] == EMPTY){
+            game->eleven->pos_x--;
         }
     }
     //DOWN
     else if(button == 'S' || button == 's'){
-        if(game->hisoka->pos_x + 1 < 15 && game->map[game->hisoka->pos_x + 1][game->hisoka->pos_y] == EMPTY){
-            game->hisoka->pos_x++;
+        if(game->eleven->pos_x + 1 < 10 && game->empty_room[game->eleven->pos_x + 1][game->eleven->pos_y] == EMPTY){
+            game->eleven->pos_x++;
         }
     }
     //LEFT
     else if(button == 'A' || button == 'a'){
-        if(game->hisoka->pos_y - 1 >= 0 && game->map[game->hisoka->pos_x][game->hisoka->pos_y - 1] == EMPTY){
-            game->hisoka->pos_y--;
+        if(game->eleven->pos_y - 1 >= 0 && game->empty_room[game->eleven->pos_x][game->eleven->pos_y - 1] == EMPTY){
+            game->eleven->pos_y--;
         }
     }
     //RIGHT
     else if(button == 'D' || button == 'd'){
-        if(game->hisoka->pos_y + 1 < 15 && game->map[game->hisoka->pos_x][game->hisoka->pos_y + 1] == EMPTY){
-            game->hisoka->pos_y++;
+        if(game->eleven->pos_y + 1 < 10 && game->empty_room[game->eleven->pos_x][game->eleven->pos_y + 1] == EMPTY){
+            game->eleven->pos_y++;
         }
     }
 
-    game->map[game->hisoka->pos_x][game->hisoka->pos_y] = HISOKA;
+    game->empty_room[game->eleven->pos_x][game->eleven->pos_y] = ELEVEN;
     
 }
 
-void gon_movement(Game* game){
-    game->map[game->gon->pos_x][game->gon->pos_y] = EMPTY;
+/*
+void will_movement(Game* game){
+    game->map[game->will->pos_x][game->will->pos_y] = EMPTY;
 
         int dash_run = rand() % 11;
         int next_movement = rand() % 4;
@@ -204,57 +241,57 @@ void gon_movement(Game* game){
         if(dash_run == 0){
             //UP DASH
             if(next_movement == 0){
-                if(game->gon->pos_x - 2 >= 0 && game->map[game->gon->pos_x - 2][game->gon->pos_y] == EMPTY){
-                    game->gon->pos_x = game->gon->pos_x - 2;
+                if(game->will->pos_x - 2 >= 0 && game->map[game->will->pos_x - 2][game->will->pos_y] == EMPTY){
+                    game->will->pos_x = game->will->pos_x - 2;
                 }
             }
             //DOWN DASH
             else if(next_movement == 1){
-                if(game->gon->pos_x + 2 < 15 && game->map[game->gon->pos_x + 2][game->gon->pos_y] == EMPTY){
-                    game->gon->pos_x = game->gon->pos_x + 2;
+                if(game->will->pos_x + 2 < 15 && game->map[game->will->pos_x + 2][game->will->pos_y] == EMPTY){
+                    game->will->pos_x = game->will->pos_x + 2;
                 }
             }
             //LEFT DASH
             else if(next_movement == 2){
-                if(game->gon->pos_y - 2 >= 0 && game->map[game->gon->pos_x][game->gon->pos_y - 2] == EMPTY){
-                    game->gon->pos_y = game->gon->pos_y - 2;
+                if(game->will->pos_y - 2 >= 0 && game->map[game->will->pos_x][game->will->pos_y - 2] == EMPTY){
+                    game->will->pos_y = game->will->pos_y - 2;
                 }
             }
             //RIGHT DASH
             else if(next_movement == 3){
-                if(game->gon->pos_y + 2 < 15 && game->map[game->gon->pos_x][game->gon->pos_y + 2] == EMPTY){
-                    game->gon->pos_y = game->gon->pos_y + 2;
+                if(game->will->pos_y + 2 < 15 && game->map[game->will->pos_x][game->will->pos_y + 2] == EMPTY){
+                    game->will->pos_y = game->will->pos_y + 2;
                 }
             }
         }
         else{
             //NORMAL UP MOVEMENT
             if(next_movement == 0){
-                if(game->gon->pos_x - 1 >= 0 && game->map[game->gon->pos_x - 1][game->gon->pos_y] == EMPTY){
-                    game->gon->pos_x--;
+                if(game->will->pos_x - 1 >= 0 && game->map[game->will->pos_x - 1][game->will->pos_y] == EMPTY){
+                    game->will->pos_x--;
                 }
             }
             //NORMAL DOWN MOVEMENT
             else if(next_movement == 1){
-                if(game->gon->pos_x + 1 < 15 && game->map[game->gon->pos_x + 1][game->gon->pos_y] == EMPTY){
-                    game->gon->pos_x++;
+                if(game->will->pos_x + 1 < 15 && game->map[game->will->pos_x + 1][game->will->pos_y] == EMPTY){
+                    game->will->pos_x++;
                 }
             }
             //NORMAL LEF MOVEMENT
             else if(next_movement == 2){
-                if(game->gon->pos_y - 1 >= 0 && game->map[game->gon->pos_x][game->gon->pos_y - 1] == EMPTY){
-                    game->gon->pos_y--;
+                if(game->will->pos_y - 1 >= 0 && game->map[game->will->pos_x][game->will->pos_y - 1] == EMPTY){
+                    game->will->pos_y--;
                 }
             }
             //NORMAL RIGHT MOVEMENT
             else if(next_movement == 3){
-                if(game->gon->pos_y + 1 < 15 && game->map[game->gon->pos_x][game->gon->pos_y + 1] == EMPTY){
-                    game->gon->pos_y++;
+                if(game->will->pos_y + 1 < 15 && game->map[game->will->pos_x][game->will->pos_y + 1] == EMPTY){
+                    game->will->pos_y++;
                 }
             }
         }
 
-    game->map[game->gon->pos_x][game->gon->pos_y] = GON;
+    game->map[game->will->pos_x][game->will->pos_y] = WILL;
 }
 
 void chrollo_movement(Game* game){
@@ -267,32 +304,32 @@ void chrollo_movement(Game* game){
 
 
     if(random_precision == 1){
-        if(game->chrollo->pos_x < game->hisoka->pos_x && game->chrollo->pos_y < game->hisoka->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y + 1] == EMPTY){
+        if(game->chrollo->pos_x < game->eleven->pos_x && game->eleven->pos_y < game->eleven->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y + 1] == EMPTY){
             game->chrollo->pos_x++;
             game->chrollo->pos_y++;
         }
-        else if(game->chrollo->pos_x < game->hisoka->pos_x && game->chrollo->pos_y > game->hisoka->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y - 1] == EMPTY){
+        else if(game->chrollo->pos_x < game->eleven->pos_x && game->chrollo->pos_y > game->eleven->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y - 1] == EMPTY){
             game->chrollo->pos_x++;
             game->chrollo->pos_y--;
         }
-        else if(game->chrollo->pos_x > game->hisoka->pos_x && game->chrollo->pos_y > game->hisoka->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y - 1] == EMPTY){
+        else if(game->chrollo->pos_x > game->eleven->pos_x && game->chrollo->pos_y > game->eleven->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y - 1] == EMPTY){
             game->chrollo->pos_x--;
             game->chrollo->pos_y--;
         }
-        else if(game->chrollo->pos_x > game->hisoka->pos_x && game->chrollo->pos_y <  game->hisoka->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y + 1] == EMPTY){
+        else if(game->chrollo->pos_x > game->eleven->pos_x && game->chrollo->pos_y <  game->eleven->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y + 1] == EMPTY){
             game->chrollo->pos_x--;
             game->chrollo->pos_y++;
         }
-        else if(game->chrollo->pos_x == game->hisoka->pos_x && game->chrollo->pos_y > game->hisoka->pos_y && game->map[game->chrollo->pos_x][game->chrollo->pos_y - 1] == EMPTY){
+        else if(game->chrollo->pos_x == game->eleven->pos_x && game->chrollo->pos_y > game->eleven->pos_y && game->map[game->chrollo->pos_x][game->chrollo->pos_y - 1] == EMPTY){
             game->chrollo->pos_y--;
         }
-        else if(game->chrollo->pos_x == game->hisoka->pos_x && game->chrollo->pos_y < game->hisoka->pos_y && game->map[game->chrollo->pos_x][game->chrollo->pos_y + 1] == EMPTY){
+        else if(game->chrollo->pos_x == game->eleven->pos_x && game->chrollo->pos_y < game->eleven->pos_y && game->map[game->chrollo->pos_x][game->chrollo->pos_y + 1] == EMPTY){
             game->chrollo->pos_y++;
         }
-        else if(game->chrollo->pos_x < game->hisoka->pos_x && game->chrollo->pos_y == game->chrollo->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y] == EMPTY){
+        else if(game->chrollo->pos_x < game->eleven->pos_x && game->chrollo->pos_y == game->chrollo->pos_y && game->map[game->chrollo->pos_x + 1][game->chrollo->pos_y] == EMPTY){
             game->chrollo->pos_x++;
         }
-        else if(game->chrollo->pos_x > game->hisoka->pos_x && game->chrollo->pos_y == game->chrollo->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y] == EMPTY){
+        else if(game->chrollo->pos_x > game->eleven->pos_x && game->chrollo->pos_y == game->chrollo->pos_y && game->map[game->chrollo->pos_x - 1][game->chrollo->pos_y] == EMPTY){
             game->chrollo->pos_x--;
         }
     }
@@ -316,10 +353,11 @@ void chrollo_movement(Game* game){
     game->map[game->chrollo->pos_x][game->chrollo->pos_y] = CHROLLO;
 
 }
+*/
 
-Game* starting_game(Game* game, Hisoka* hisoka, Gon* gon, Chrollo* chrollo){
-    game->hisoka = hisoka;
-    game->gon = gon;
+Game* starting_game(Game* game, Eleven* eleven, Will* will, Chrollo* chrollo){
+    game->eleven = eleven;
+    game->will = will;
     game->chrollo = chrollo;
 }
 
@@ -329,34 +367,42 @@ int main(){
     srand(time(NULL));
 
     //important variables
-    char** map;
-    Hisoka *hisoka;
-    Gon *gon;
+    char** upside_down;
+    char** empty_room;
+    Eleven *eleven;
+    Will *will;
     Chrollo *chrollo;
     Game *game;
     int EOG = 1;
 
-    gon = (Gon*) malloc(1 * sizeof(Gon));
-    gon = create_gon(gon);
+    will = (Will*) malloc(1 * sizeof(Will));
+    will = create_will(will);
 
-    hisoka = (Hisoka*) malloc(1 * sizeof(Hisoka));
-    hisoka = create_hisoka(hisoka);
+    eleven = (Eleven*) malloc(1 * sizeof(Eleven));
+    eleven = create_eleven(eleven);
 
     chrollo = (Chrollo*) malloc(1 * sizeof(Chrollo));
-    chrollo = create_chrollo(chrollo, gon);
+    chrollo = create_chrollo(chrollo, will);
     
 
     game = (Game*) malloc(1 * sizeof(Game));
-    game = starting_game(game, hisoka, gon, chrollo);
+    game = starting_game(game, eleven, will, chrollo);
 
-    map_generator(game);
-    show_map(game);
+    //upside_down_generator(game);
+    //show_upside_down(game);
+    empty_room_generator(game);
+    show_empty_room(game);
+
+    //map_generator(game);
+    //show_map(game);
 
     while(EOG == 1){
-        gon_movement(game);
-        hisoka_movement(game);
-        chrollo_movement(game);
-        show_map(game);
+        //will_movement(game);
+        eleven_movement(game);
+        //chrollo_movement(game);
+        //show_map(game);
+        //show_upside_down(game);
+        show_empty_room(game);
     }
 
 
